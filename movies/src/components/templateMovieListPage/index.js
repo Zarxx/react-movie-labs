4f55e99863ebe38cfc4b8cf67ab809d9d55ef1e9
Adrian <20094388@mail.wit.ie>
2024-03-26 19:21:21 +0000

    debugging attempt

--- movies/src/components/templateMovieListPage/index.js
+++ movies/src/components/templateMovieListPage/index.js
@@ -33,12 +33,9 @@ function MovieListPageTemplate({ movies, title, action }) {
       <Grid item container spacing={5}>
         <Grid key="find" item xs={12} sm={6} md={4} lg={3} xl={2}>
           <FilterCard
-            onUserInput={handleChange}
-            titleFilter={nameFilter}
-            genreFilter={genreFilter}
-            releaseYearFilter={releaseYearFilter}
-            setReleaseYearFilter={setReleaseYearFilter}
+            setNameFilter={setNameFilter}
             setGenreFilter={setGenreFilter}
+            setReleaseYearFilter={setReleaseYearFilter}
           />
         </Grid>
         <MovieList action={action} movies={displayedMovies}></MovieList>
