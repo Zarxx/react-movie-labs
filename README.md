79e7efca318db11c0a4bded11257ccf17e3de732
Adrian <20094388@mail.wit.ie>
2024-03-31 17:33:51 +0100

    updated readme.

--- README.md
+++ README.md
@@ -1,48 +1,30 @@
 # Assignment 1 - ReactJS app.
 
-Name: [your name]
+Name: Adrian Jasik
 
 ## Overview.
 
-[A brief statement on the content of this repository.]
+This is a react movie app. It has a variety of features related to movies such as filtering movies by genre, different pages listing popular, upcoming, top rated movies etc. Among other features.
 
 ### Features.
 
-[ A bullet-point list of the __new features__ you added to the Movies Fan app (and any modifications to existing features) .]
-
-- Feature 1
-- Feature 2
-- Feature 3
-- etc
-- etc
+- Popular Movies Page
+- Top Rated Movies Page
+- Filter by year (not functional)
+- New Material Ui components
+- Caching
 
 ## Setup requirements.
 
-[ Outline any non-standard setup steps necessary to run your app locally after cloning the repo.]
+---
 
 ## API endpoints.
 
-[ List the __additional__ TMDB endpoints used, giving the description and pathname for each one.]
-
-e.g.
-
-- Discover list of movies - discover/movie
-- Movie details - movie/:id
-- Movie genres = /genre/movie/list
+- List of popular movies - components/pages/popularMoviesPage.js
+- List of top rated movies - components/pages/topRatedMoviesPage.js
+- Get Release Years - components/filterMoviesCard/index.js
 
 ## Routing.
 
-[ List the __new routes__ supported by your app and state the associated page.]
-
-- /blogs - displays all published blogs.
-- /blogs/:id - displays a particular blog.
-- /blogs/:id/comments - detail view of a particular blog and its comments.
-- etc.
-
-[If relevant, state what aspects of your app are protected (i.e. require authentication) and what is public.]
-
-## Independent learning (If relevant).
-
-Itemize the technologies/techniques you researched independently and adopted in your project,
-i.e. aspects not covered in the lectures/labs. Include the source code filenames that illustrate these
-(we do not require code excerpts) and provide references to the online resources that helped you (articles/blogs).
+-/movies/popular - displays popular movies
+-/movies/toprated - displays top rated movies
--- movies/src/components/filterMoviesCard/index.js
+++ movies/src/components/filterMoviesCard/index.js
@@ -1,4 +1,4 @@
-import React, {useState}  from "react";
+import React, { useState } from "react";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
@@ -9,10 +9,10 @@ import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-import img from '../../images/pexels-dziana-hasanbekava-5480827.jpg'
+import img from "../../images/pexels-dziana-hasanbekava-5480827.jpg";
 import { getGenres, getReleaseYears } from "../../api/tmdb-api";
 import { useQuery } from "react-query";
-import Spinner from '../spinner'
+import Spinner from "../spinner";
 
 const formControl = {
   margin: 1,
@@ -22,139 +22,122 @@ const formControl = {
 
 export default function FilterMoviesCard(props) {
   const { setNameFilter, setGenreFilter, setReleaseYearFilter } = props;
-const [releaseYear, setReleaseYear] = useState("");
-const { data, error, isLoading, isError } = useQuery("genres", getGenres);
+  const [releaseYear, setReleaseYear] = useState("");
+  const { data, error, isLoading, isError } = useQuery("genres", getGenres);
 
+  const handleChange = (type, value) => {
+    if (type === "name") setNameFilter(value);
+    else if (type === "genre") setGenreFilter(value);
+    else if (type === "releaseYear") setReleaseYearFilter(value);
+  };
 
+  const handleTextChange = (e) => {
+    handleChange(e, "name", e.target.value);
+  };
 
-const handleChange = (type, value) => {
-  if (type === "name") setNameFilter(value);
-  else if (type === "genre") setGenreFilter(value);
-  else if (type === "releaseYear") setReleaseYearFilter(value);
-};
+  const handleGenreChange = (e) => {
+    handleChange(e, "genre", e.target.value);
+  };
 
-const handleTextChange = (e) => {
-  handleChange(e, "name", e.target.value);
-};
+  const handleYearChange = (e) => {
+    setReleaseYear(e.target.value);
+  };
 
-const handleGenreChange = (e) => {
-  handleChange(e, "genre", e.target.value);
-};
+  const handleReleaseYearChange = (e) => {
+    const selectedYear = e.target.value;
+    setReleaseYear(selectedYear); //updates local state
+    setReleaseYearFilter(selectedYear); //updates prop
+  };
 
-const handleYearChange = (e) => {
-  setReleaseYear(e.target.value);
-};
+  if (isLoading) {
+    return <Spinner />;
+  }
 
-const handleReleaseYearChange = (e) => {
-  const selectedYear = e.target.value;
-  setReleaseYear(selectedYear);        //updates local state
-  setReleaseYearFilter(selectedYear);  //updates prop
-};
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
 
-
-if (isLoading) {
-  return <Spinner />;
-}
-
-if (isError) {
-  return <h1>{error.message}</h1>;
-}
-
-const genres = data.genres;
+  const genres = data.genres;
   if (genres[0].name !== "All") {
     genres.unshift({ id: "0", name: "All" });
   }
 
-
-
-
-
   return (
-    <Card 
-    sx={{
-      maxWidth: 345,
-      backgroundColor: "rgb(204, 204, 0)"
-    }} 
-    variant="outlined">
-    <CardContent>
-      <Typography variant="h5" component="h1">
-        <SearchIcon fontSize="large" />
-        Filter the movies.
-      </Typography>
-      <TextField
-      sx={{...formControl}}
-      id="filled-search"
-      label="Search field"
-      type="search"
-      variant="filled"
-      value={props.titleFilter}
-      onChange={(e) => setNameFilter(e.target.value)}
-    />
-        <FormControl sx={{...formControl}}>
-        <InputLabel id="genre-label">Genre</InputLabel>
-        <Select
-          labelId="genre-label"
-          id="genre-select"
-          defaultValue=""
-          value={props.genreFilter}
-          onChange={(e) => setGenreFilter(e.target.value)}
-   >
+    <Card
+      sx={{
+        maxWidth: 345,
+        backgroundColor: "rgb(204, 204, 0)",
+      }}
+      variant="outlined"
+    >
+      <CardContent>
+        <Typography variant="h5" component="h1">
+          <SearchIcon fontSize="large" />
+          Filter the movies.
+        </Typography>
+        <TextField
+          sx={{ ...formControl }}
+          id="filled-search"
+          label="Search field"
+          type="search"
+          variant="filled"
+          value={props.titleFilter}
+          onChange={(e) => setNameFilter(e.target.value)}
+        />
+        <FormControl sx={{ ...formControl }}>
+          <InputLabel id="genre-label">Genre</InputLabel>
+          <Select
+            labelId="genre-label"
+            id="genre-select"
+            defaultValue=""
+            value={props.genreFilter}
+            onChange={(e) => setGenreFilter(e.target.value)}
+          >
             {genres.map((genre) => {
-            return (
-              <MenuItem key={genre.id} value={genre.id}>
-                {genre.name}
-              </MenuItem>
-            );
-          })}
-        </Select>
-      </FormControl>
+              return (
+                <MenuItem key={genre.id} value={genre.id}>
+                  {genre.name}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
 
+        <FormControl sx={{ ...formControl }}>
+          <InputLabel id="release-year-label">Release Year</InputLabel>
 
-
-
-<FormControl sx={{ ...formControl }}>
-<InputLabel id="release-year-label">Release Year</InputLabel>
-
-<Select
-    labelId="release-year-label"
-    id="release-year-select"
-    value={props.releaseYearFilter}
-    onChange={handleReleaseYearChange}
-  >
-
-<MenuItem value="">All</MenuItem>
-<MenuItem value="2024">2024</MenuItem>
-<MenuItem value="2023">2023</MenuItem>
-<MenuItem value="2023">2022</MenuItem>
-<MenuItem value="2023">2021</MenuItem>
-<MenuItem value="2023">2020</MenuItem>
-<MenuItem value="2023">2019</MenuItem>
-<MenuItem value="2023">2018</MenuItem>
-<MenuItem value="2023">2017</MenuItem>
-<MenuItem value="2023">2016</MenuItem>
-<MenuItem value="2023">2015</MenuItem>
-<MenuItem value="2023">2014</MenuItem>
-<MenuItem value="2023">2013</MenuItem>
-<MenuItem value="2023">2012</MenuItem>
-<MenuItem value="2023">2011</MenuItem>
-<MenuItem value="2023">2010</MenuItem>
-<MenuItem value="2023">2009</MenuItem>
-<MenuItem value="2023">2008</MenuItem>
-<MenuItem value="2023">2007</MenuItem>
-<MenuItem value="2023">2006</MenuItem>
-<MenuItem value="2023">2005</MenuItem>
-<MenuItem value="2023">2004</MenuItem>
-
-
-</Select>
-</FormControl>
-
-</CardContent>
-      <CardMedia
-        sx={{ height: 300 }}
-        image={img}
-        title="Filter"
-      />
+          <Select
+            labelId="release-year-label"
+            id="release-year-select"
+            value={props.releaseYearFilter}
+            onChange={handleReleaseYearChange}
+          >
+            <MenuItem value="">All</MenuItem>
+            <MenuItem value="2024">2024</MenuItem>
+            <MenuItem value="2023">2023</MenuItem>
+            <MenuItem value="2022">2022</MenuItem>
+            <MenuItem value="2021">2021</MenuItem>
+            <MenuItem value="2020">2020</MenuItem>
+            <MenuItem value="2019">2019</MenuItem>
+            <MenuItem value="2018">2018</MenuItem>
+            <MenuItem value="2017">2017</MenuItem>
+            <MenuItem value="2016">2016</MenuItem>
+            <MenuItem value="2015">2015</MenuItem>
+            <MenuItem value="2014">2014</MenuItem>
+            <MenuItem value="2013">2013</MenuItem>
+            <MenuItem value="2012">2012</MenuItem>
+            <MenuItem value="2011">2011</MenuItem>
+            <MenuItem value="2010">2010</MenuItem>
+            <MenuItem value="2009">2009</MenuItem>
+            <MenuItem value="2008">2008</MenuItem>
+            <MenuItem value="2007">2007</MenuItem>
+            <MenuItem value="2006">2006</MenuItem>
+            <MenuItem value="2005">2005</MenuItem>
+            <MenuItem value="2004">2004</MenuItem>
+          </Select>
+        </FormControl>
+      </CardContent>
+      <CardMedia sx={{ height: 300 }} image={img} title="Filter" />
       <CardContent>
         <Typography variant="h5" component="h1">
           <SearchIcon fontSize="large" />
